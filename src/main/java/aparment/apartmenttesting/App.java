1ca64af2983c4aca0e6bfb43920957472af19505
AD <AD@ADs-MacBook-Pro.local>
2017-03-18 13:27:01 -0500

    Initialize project

---
+++ src/main/java/aparment/apartmenttesting/App.java
@@ -0,0 +1,13 @@
+package aparment.apartmenttesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
